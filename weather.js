6fa2c480fde71d8b6492a138b86f4dcbe53b6bb0
Anchit402 <55657967+Anchit402@users.noreply.github.com>
2020-12-28 12:36:52 +0530

    Update weather.js

--- weather.js
+++ weather.js
@@ -1,32 +1,23 @@
 var search = document.querySelector('.search input');
 var btn = document.querySelector('button');
 btn.addEventListener('click', () => {
-	fetch('http://api.weatherstack.com/current?access_key=8344ce2e5e07a7caca0f134d704e7ef5&query='+search.value).then((res) => {
+	fetch('https://api.mapbox.com/geocoding/v5/mapbox.places/'+search.value+'.json?access_token=pk.eyJ1IjoiYW5jaGl0YWdhcndhbCIsImEiOiJjazluOTI5NXcwNHFkM2dwY3lzYnBnaXhoIn0.LyMxtcAV03F9eqQCMc4PyQ&unit=1').then((res) => {
+		return res.json();
+		}).then((data) => {
+		const long = data.features[0].center[0];
+		const lat = data.features[0].center[1];
+		console.log(data.features[0].place_name);
+		var name = document.querySelector('#name');
+		name.textContent = data.features[0].place_name;
+		console.log(long, lat);
+		fetch('http://api.openweathermap.org/data/2.5/weather?lat='+lat+'&lon='+long+'&appid=ac7c75b9937a495021393024d0a90c44&unit=metric').then((res) => {
 		return res.json();
 	}).then((data) => {
-		var name = document.querySelector('#name');
-		name.textContent = data.location.country + ', ' + data.location.region;
-		var temp = document.querySelector('#temp');
-		temp.textContent = data.current.temperature;
-		var desc = document.querySelector('#desc');
-		desc.textContent = data.current.weather_descriptions[0];
-		var humid = document.querySelector('#humid');
-		humid.textContent = data.current.humidity;
-		var ws = document.querySelector('#ws');
-		ws.textContent = data.current.wind_speed;
-		var isd = document.querySelector('#isd');
-		if(data.current.is_day === 'yes')
-		{
-			isd.textContent = 'day';
-		}
-		else
-		{
-			isd.textContent = 'night';
-		}
-		
-		var ps = document.querySelector('#ps');
-		ps.textContent = data.current.precip;
-		var prs = document.querySelector('#prs');
-		prs.textContent = data.current.pressure;
-	})
+		console.log(data.weather[0].description);
+		console.log(data.main.temp - 272.15);
+		console.log(data.main.humidity);
+		console.log(data.main.pressure);
+		console.log(data);
+	})	
 })
+	})
