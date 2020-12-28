477416b98817202dff3aeec31e497a4df1aa41bb
Anchit402 <55657967+Anchit402@users.noreply.github.com>
2020-12-28 11:50:15 +0530

    Update weather.js

--- weather.js
+++ weather.js
@@ -1,19 +1,11 @@
 var search = document.querySelector('.search input');
 var btn = document.querySelector('button');
 btn.addEventListener('click', () => {
-	fetch('https://api.mapbox.com/geocoding/v5/mapbox.places/'+search.value+'.json?access_token=pk.eyJ1IjoiYW5jaGl0YWdhcndhbCIsImEiOiJjazluOTI5NXcwNHFkM2dwY3lzYnBnaXhoIn0.LyMxtcAV03F9eqQCMc4PyQ&unit=1').then((res) => {
-		return res.json();
-		}).then((data) => {
-		const long = data.features[0].center[0];
-		const lat = data.features[0].center[1];
-		console.log(data.features[0].place_name);
-		var name = document.querySelector('#name');
-		name.textContent = data.features[0].place_name;
-		console.log(long, lat);
-		fetch('http://api.weatherstack.com/current?access_key=8344ce2e5e07a7caca0f134d704e7ef5&query='+lat+','+long+'&unit=m').then((res) => {
+	fetch('http://api.weatherstack.com/current?access_key=8344ce2e5e07a7caca0f134d704e7ef5&query='+search.value).then((res) => {
 		return res.json();
 	}).then((data) => {
-		console.log(data.current.temperature);
+		var name = document.querySelector('#name');
+		name.textContent = data.location.country + ', ' + data.location.region;
 		var temp = document.querySelector('#temp');
 		temp.textContent = data.current.temperature;
 		var desc = document.querySelector('#desc');
@@ -37,5 +29,4 @@ btn.addEventListener('click', () => {
 		var prs = document.querySelector('#prs');
 		prs.textContent = data.current.pressure;
 	})
- })
-	})
+})
